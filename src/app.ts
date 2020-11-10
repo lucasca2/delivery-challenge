99e81610d01634ab0f88050574c68eb73bb2ff70
Lucas Amaral <lucas.costamaral@gmail.com>
2020-11-10 20:02:35 -0300

    chore(config): Adicionar middlewares padrão
    Adicionar no servidor as middlewares de cors e json do express.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express, { Application } from 'express'
+import cors from 'cors'
 
 class App {
   public server: Application;
 
   public constructor () {
     this.server = express()
+
+    this.middlewares()
+  }
+
+  private middlewares () {
+    this.server.use(express.json())
+    this.server.use(cors())
   }
 }
 
