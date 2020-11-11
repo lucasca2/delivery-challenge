b40d368899b387ae5b191518c321d2dfcb3e5d5d
Lucas Amaral <lucas.costamaral@gmail.com>
2020-11-11 17:55:26 -0300

    refactor(service): Reajuste no service do RecipePuppy
    Refazer o tratamento dos parametros para formatar o array e transformar em string

--- src/app/services/RecipePuppy.ts
+++ src/app/services/RecipePuppy.ts
@@ -21,8 +21,9 @@ class RecipePuppy {
     this.api = axios.create({ baseURL: process.env.RECIPE_PUPPY_API })
   }
 
-  public async getRecipes (igredients: string): Promise<ResponseRecipePuppy> {
-    const { data } = await this.api.get<ResponseRecipePuppy>('/', { params: { i: igredients } })
+  public async getRecipes (igredients: any): Promise<ResponseRecipePuppy> {
+    const i = igredients.join(',')
+    const { data } = await this.api.get<ResponseRecipePuppy>('/', { params: { i } })
 
     return data
   }
