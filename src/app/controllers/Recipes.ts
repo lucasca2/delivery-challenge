2fa90e4045519a4cd5fe8f0c2a62273e6ab36be2
Lucas Amaral <lucas.costamaral@gmail.com>
2020-11-11 17:57:36 -0300

    refactor(controller): Reajuste na listagem de receitas
    Remover parte da validação duplicada pois agora está em um arquivo separado

--- src/app/controllers/Recipes.ts
+++ src/app/controllers/Recipes.ts
@@ -3,19 +3,15 @@ import RecipePuppy from '../services/RecipePuppy'
 
 class Recipes {
   public async index (req: Request, res: Response): Promise<Response> {
-    let { igredients } = req.query || {}
-
     try {
-      if (!igredients) return res.status(400).json({ error: 'You need to inform the igredients' })
-      if (Array.isArray(igredients)) igredients = igredients.join(',')
-      if (typeof igredients === 'string') {
-        const response = await RecipePuppy.getRecipes(igredients)
+      const { i: igredients } = req.query
 
-        return res.json({
-          keywords: igredients.split(/,\s*/),
-          recipes: response.results
-        })
-      }
+      const response = await RecipePuppy.getRecipes(igredients)
+
+      return res.json({
+        keywords: igredients,
+        recipes: response
+      })
     } catch (e) {
       return res.json(e)
     }
